b13828b04c1b97b9084f0cb33b71f907853fb2bf
alexarlord-boop <rezerw0.1@yandex.ru>
2020-11-21 12:34:58 +0300

    TRIANGLE-1022 Added base structure

---
+++ leetcode/triangle/triangle.py
@@ -0,0 +1,22 @@
+import sys
+
+
+class Triangle():
+    pass
+
+
+def read_data():
+    pass
+
+
+def write_data():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    params = sys.argv
+    main(params[1], params[2])
