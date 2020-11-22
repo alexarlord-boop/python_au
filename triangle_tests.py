0df7786931bf21ef85345f50613cae5be8de19fc
alexarlord-boop <rezerw0.1@yandex.ru>
2020-11-22 11:05:21 +0300

    TRIANGLE_TEST-1022 Added point method test

--- triangle_tests.py
+++ triangle_tests.py
@@ -3,7 +3,10 @@ from triangle import Point, Triangle, TaskSolver
 
 
 class TestPointAddMethod(unittest.TestCase):
-    pass
+    def test_add(self):
+        self.assertEqual(Point(2, 3) + Point(3, 3), (5, 6))
+        self.assertEqual(Point(2.5, 3.0) + Point(1.2, 3.2), (3.7, 6.2))
+        self.assertEqual(Point(-2, -3) + Point(2, 3), (0, 0))
 
 
 class TestTriangleMethods(unittest.TestCase):
