a13faa8f77f96c42a9ee1cd2f4de0c28ccc9e53d
alexarlord-boop <rezerw0.1@yandex.ru>
2020-11-22 11:04:39 +0300

    TRIANGLE_TEST-1022 Added triangle_tests with base structure

---
+++ triangle_tests.py
@@ -0,0 +1,18 @@
+import unittest
+from triangle import Point, Triangle, TaskSolver
+
+
+class TestPointAddMethod(unittest.TestCase):
+    pass
+
+
+class TestTriangleMethods(unittest.TestCase):
+    pass
+
+
+class TestTaskSolverMethods(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
