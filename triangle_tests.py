e613e46b95f3813978603727e2021ec7494d4d53
alexarlord-boop <rezerw0.1@yandex.ru>
2020-11-22 13:09:53 +0300

    TRIANGLE_TEST-1022 Refactored for pep8

--- triangle_tests.py
+++ triangle_tests.py
@@ -73,7 +73,7 @@ class TestTaskSolverMethods(unittest.TestCase):
     def test_parse_data(self):
         task_solver = TaskSolver('in.txt', 'out.txt')
         task_solver.read_data()
-        self.assertEqual(task_solver.parse_data(), [[0, 3, 0, 4, 0, 0.07,],
+        self.assertEqual(task_solver.parse_data(), [[0, 3, 0, 4, 0, 0.07, ],
                                                     [0, 0, 0, 3, 3, 0]])
 
         task_solver = TaskSolver('in2.txt', 'out.txt')
@@ -95,7 +95,6 @@ class TestTaskSolverMethods(unittest.TestCase):
         task_solver.read_data()
         task_solver.parse_data()
         self.assertEqual(task_solver.find_largest_triangle(), 0)
-
 
 
 if __name__ == '__main__':
