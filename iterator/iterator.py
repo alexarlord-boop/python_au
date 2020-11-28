ebc8f144baa04e8b77a1d4229a6eb4348bd4a22f
alexarlord-boop <rezerw0.1@yandex.ru>
2020-11-28 13:28:59 +0300

    ITERATOR-1022 Refactored node class

--- iterator/iterator.py
+++ iterator/iterator.py
@@ -1,7 +1,10 @@
 class node:
     def __init__(self, val):
-        self.val = val
+        self.val = {"name": val[0], "group": val[1], "subgroup": val[2]}
         self.next = None
+
+    def __str__(self):
+        return f"Name: {self.val['name']} Group: {self.val['group']} Subgroup: {self.val['subgroup']}"
 
 
 class MyLinkedList:
@@ -22,20 +25,24 @@ class MyLinkedList:
         else:
             raise StopIteration
 
-    def get(self, index: int) -> int:
+    def printlist(self):
+        for elem in self:
+            print(elem)
+
+    def get(self, index) -> int:
         if index < len(self.lst):
-            return self.lst[index].val
+            return f"{index}. " + str(self.lst[index])
         else:
             return -1
 
-    def addAtHead(self, val: int) -> None:
+    def addAtHead(self, val) -> None:
         new_node = node(val)
         new_node.next = self.head
         self.head = new_node
         self.lst.insert(0, self.head)
         self.limit += 1
 
-    def addAtTail(self, val: int) -> None:
+    def addAtTail(self, val) -> None:
         new_node = node(val)
 
         if len(self.lst) == 0:
@@ -48,7 +55,7 @@ class MyLinkedList:
         self.lst.append(new_node)
         self.limit += 1
 
-    def addAtIndex(self, index: int, val: int) -> None:
+    def addAtIndex(self, index: int, val) -> None:
         new_node = node(val)
         if index == 0:
             self.lst.insert(index, new_node)
@@ -80,6 +87,7 @@ class MyLinkedList:
 
             self.limit -= 1
 
+
 # Your MyLinkedList object will be instantiated and called as such:
 # obj = MyLinkedList()
 # param_1 = obj.get(index)
@@ -87,3 +95,17 @@ class MyLinkedList:
 # obj.addAtTail(val)
 # obj.addAtIndex(index,val)
 # obj.deleteAtIndex(index)
+
+ml = MyLinkedList()
+ml.addAtHead(['A', 102, 2])
+ml.addAtHead(['B', 102, 2])
+ml.addAtHead(['C', 102, 1])
+
+# print(next(ml))
+# print(next(ml))
+# print(next(ml))
+
+# for student in ml:
+#     print(student)
+
+# ml.printlist()
