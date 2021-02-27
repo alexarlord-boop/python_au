469712b8a5bde70c03545b55ba052d8a80f123c3
alexarlord-boop <rezerw0.1@yandex.ru>
2021-02-27 13:09:57 +0300

    REQUESTS-1022 Added get_pulls function

--- requests/check_pull.py
+++ requests/check_pull.py
@@ -11,31 +11,42 @@ def get_git_names():
     return names
 
 
-class Validator():  # for one user
+class Validator:  # for one user
     def __init__(self, name):
         self.name = name
         self.repos = list()
-        self.get_user_repos()
+        # self.get_user_repos()
         self.curr_repo = None
+        self.TOKEN =
 
-    def get_user_repos(self):
-        g = Github()
-        user = g.get_user(self.name)
-
-        self.repos = user.get_repos()
+    # def get_user_repos(self):
+    #     g = Github()
+    #     user = g.get_user(self.name, )
+    #     self.repos = user.get_repos()
 
     def get_spec_repo(self, repo_name):
         for repo in self.repos:
-            if repo.name == 'python_au':
+            if repo.name == repo_name:
                 self.curr_repo = repo
+
+    def prepare_headers(self):
+        return {
+            'Autorization': 'token {}'.format(self.TOKEN),
+            'Content-Type': "application/json",
+            'Accept': "application/vnd.github.v3+json"
+        }
+
+    def get_pulls(self, repo_name):
+        self.get_spec_repo(repo_name)
+        url = f'https://api.github.com/repos/{self.name}/{repo_name}/pulls'
+        r = requests.get(url, headers=self.prepare_headers())
+        for pull in r.json():
+            print(pull['title'])
 
 
 if __name__ == '__main__':
     usernames = get_git_names()
     for name in usernames:
         validator = Validator(name)
-
-
-        for rep in validator.repos:
-            print(rep)
+        validator.get_pulls('python_au')
         print('--------------------')
