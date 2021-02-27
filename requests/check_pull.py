73f41567be3cc7de99e65f98a872b04119ad319c
alexarlord-boop <rezerw0.1@yandex.ru>
2021-02-27 16:35:50 +0300

    REQUESTS-1022 Added get_all_pulls method

--- requests/check_pull.py
+++ requests/check_pull.py
@@ -6,7 +6,7 @@ actions = ['Added', 'Deleted', 'Fixed', 'Refactored', 'Moved']
 groups = ['1022', '1021']
 
 
-def get_git_names():
+def get_git_usernames():
     names = list()
     with open('links.txt') as f:
         raw_links = f.readlines()
@@ -15,19 +15,19 @@ def get_git_names():
     return names
 
 
-class Validator:  # for one user
-    def __init__(self, name):
-        self.name = name
+class Validator:
+    def __init__(self, user):
+        self.user = user
         self.repos = list()
         # self.get_user_repos()
         self.curr_repo = None
         self.invalid_pulls = list()
-        self.TOKEN =
+        self.TOKEN = 'e94a023059fec44f0283b00f2c9505ab8999ff4b'
 
-    # def get_user_repos(self):
-    #     g = Github()
-    #     user = g.get_user(self.name, )
-    #     self.repos = user.get_repos()
+    def get_user_repos(self):
+        g = Github()
+        user = g.get_user(self.user)
+        self.repos = user.get_repos()
 
     def get_spec_repo(self, repo_name):
         for repo in self.repos:
@@ -41,13 +41,20 @@ class Validator:  # for one user
             'Accept': "application/vnd.github.v3+json"
         }
 
+    def get_all_pulls(self, repo_name):
+        url = f'https://api.github.com/repos/{self.user}/{repo_name}/pulls'
+        pulls = requests.get(url, headers=self.prepare_headers()).json()
+        return pulls
+
     def get_invalid_pulls(self, repo_name):
         self.get_spec_repo(repo_name)
-        url = f'https://api.github.com/repos/{self.name}/{repo_name}/pulls'
-        r = requests.get(url, headers=self.prepare_headers())
-        for pull in r.json():
+        invalid_pulls = list()
+        # url = f'https://api.github.com/repos/{self.name}/{repo_name}/pulls'
+        # r = requests.get(url, headers=self.prepare_headers())
+        for pull in self.get_all_pulls(repo_name):
             if not self.is_valid_pull(pull['title']):
-                self.invalid_pulls.append(pull)
+                invalid_pulls.append(pull)
+        return invalid_pulls
 
     def is_valid_pull(self, pull):
         pull_parts = pull.split()
@@ -57,13 +64,13 @@ class Validator:  # for one user
 
 
 if __name__ == '__main__':
-    usernames = get_git_names()
-    r = requests.post('https://github.com/alexarlord-boop/python_au/pull/33/comment?sticky=true', data='Fixe your pull request.')
-    print(r)
-    for name in usernames:
-        validator = Validator(name)
-        validator.get_invalid_pulls('python_au')
-        print(validator.name)
-        for pull in validator.invalid_pulls:
+    usernames = get_git_usernames()
+    for user in usernames:
+
+        validator = Validator(user)
+        invalid_pulls = validator.get_invalid_pulls('python_au')
+
+        print(validator.user)
+        for pull in invalid_pulls:
             print(pull['title'])
-            print('-'*20)
+        print('-' * 20)
