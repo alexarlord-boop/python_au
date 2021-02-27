f5720283a3f967daac710fce227d7f2725d92128
alexarlord-boop <rezerw0.1@yandex.ru>
2021-02-27 11:17:34 +0300

    REQUESTS-1022 Added Validator class

--- requests/check_pull.py
+++ requests/check_pull.py
@@ -2,20 +2,21 @@ import requests
 from github import Github
 
 
-# Github usernames
-def get_git_names():
-    names = list()
-    with open('links.txt') as f:
-        raw_links = f.readlines()
-        for link in raw_links:
-            names.append(link.split('/')[-1].strip('\n'))
-    return names
+class Validator():
+    def __init__(self):
+        self.names = list()
+        self.current_repo = list()
+        self.get_git_names()
 
+    # Github usernames
+    def get_git_names(self):
+        with open('links.txt') as f:
+            raw_links = f.readlines()
+            for link in raw_links:
+                self.names.append(link.split('/')[-1].strip('\n'))
 
-usernames = get_git_names()
-for username in usernames:
-
-    g = Github()
-    user = g.get_user(username)
-    for repo in user.get_repos():
-        print(repo)
+    def get_user_repos(self, name):
+        g = Github()
+        user = g.get_user(name)
+        for repo in user.get_repos():
+            self.current_repo.append(repo)
