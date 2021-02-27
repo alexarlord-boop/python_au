9c13dbb258d6999ff12c87f6f0da71fc2e829f4f
alexarlord-boop <rezerw0.1@yandex.ru>
2021-02-27 11:57:42 +0300

    REQUESTS-1022 Refactored Validator class

--- requests/check_pull.py
+++ requests/check_pull.py
@@ -2,21 +2,34 @@ import requests
 from github import Github
 
 
-class Validator():
-    def __init__(self):
-        self.names = list()
-        self.current_repo = list()
-        self.get_git_names()
+def get_git_names():
+    names = list()
+    with open('links.txt') as f:
+        raw_links = f.readlines()
+        for link in raw_links:
+            names.append(link.split('/')[-1].strip('\n'))
+    return names
+
+
+class Validator():  # for one user
+    def __init__(self, name):
+        self.name = name
+        self.repos = list()
 
     # Github usernames
-    def get_git_names(self):
-        with open('links.txt') as f:
-            raw_links = f.readlines()
-            for link in raw_links:
-                self.names.append(link.split('/')[-1].strip('\n'))
 
-    def get_user_repos(self, name):
+    def get_user_repos(self):
         g = Github()
-        user = g.get_user(name)
+        user = g.get_user(self.name)
+
         for repo in user.get_repos():
-            self.current_repo.append(repo)
+            self.repos.append(repo)
+
+
+if __name__ == '__main__':
+    usernames = get_git_names()
+    for name in usernames:
+        validator = Validator(name)
+        validator.get_user_repos()
+        print(validator.repos)
+        print('--------------------')
